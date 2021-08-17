466c8960496953c4b963b987b59f190fabce73aa
Marc Wiedermann <marcwiedermann@posteo.de>
2021-08-17 16:25:13 +0200

    load nuts data from eurostat servers and store into dataframe

--- pynuts/dataio.py
+++ pynuts/dataio.py
@@ -46,3 +46,59 @@ def load_lau_table(country_code):
     lau = lau[lau["CNTR_CODE"] == country_code]
 
     return lau
+
+
+def load_nuts_table(country_code, spatial_resolution, level):
+    """
+    Load a pandas data frame of nuts codes.
+
+    Args:
+        country_code: A two digit country code (str)
+        spatial_resolution: The accuracy of the shapefiles (int)
+        level: The nuts level (int)
+
+    Returns:
+        Pandas data frame with all NUTS regions in the specified country.
+
+    Loads the data from the Eurostat server into pynuts' config folder and
+    stores it there for later use.
+
+    The spatial resolution can either of five values: 1, 3, 10, 20 or 60.
+    Higher values mean coarser resolution.
+
+    The level can take either of three valuess: 1,2 or 3. They translate into
+    - NUTS 1: major socio-economic regions
+    - NUTS 2: basic regions for the application of regional policies
+    - NUTS 3: small regions for specific diagnoses
+    """
+    print("Loading NUTS data from 2021") 
+    
+    if spatial_resolution not in [1, 3, 10, 20, 60]:
+        print("spatial_resolution must be either 1, 3, 10, 20 or 60.")
+        return
+
+    if level not in [1, 2, 3]:
+        print("level must be either 1, 2 or 3.")
+        return
+
+    spatial_resolution = str(spatial_resolution).zfill(2)
+
+    # TODO: Let users choose year and version in the future
+    path = "nuts/download/"
+    file_name = f"ref-nuts-2021-{spatial_resolution}m.shp.zip"
+    shapefile = f"NUTS_RG_{spatial_resolution}M_2021_4326_LEVL_{level}.shp.zip"
+
+    # Download the data
+    if not os.path.exists(DATA_DIRECTORY+file_name):                              
+        wget.download(SERVER+path+file_name, DATA_DIRECTORY+file_name)
+  
+    # Unzip the data
+    if not os.path.exists(DATA_DIRECTORY+file_name[:-4]):
+        with zipfile.ZipFile(DATA_DIRECTORY+file_name, 'r') as zip_ref:
+            zip_ref.extractall(DATA_DIRECTORY+file_name[:-4])
+   
+    # Load data into pandas dataframe and filter for specified country
+    df = geopandas.read_file(DATA_DIRECTORY+file_name[:-4]+"/"+shapefile)
+    df = df[df["CNTR_CODE"] == country_code]
+
+    return df
